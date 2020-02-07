aecd095aa46eb7232b9d812e28c2a0013f2a71d4
Ricky Aston <rickysimonaston@gmail.com>
2020-02-07 17:18:06 +1000

    Session3:-Module:16

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
-const express = require("express");
-const dotenv = require("dotenv");
+// 3rd party requires
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+// Own App requirements
 
 //Route Files
-const bootcamps = require('./routes/bootcamps')
+const bootcamps = require('./routes/bootcamps');
 
 // Load ENV vars
 dotenv.config({
-  path: "./config/config.env"
+  path: './config/config.env',
 });
 
 // Create express app
 const app = express();
 
-// Mount routers
-app.use('/api/v1/bootcamps', bootcamps)
+// Dev logging Middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 // Create express server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(
-    `App listening on port ${process.env.NODE_ENV} mode on port ${PORT}!`
+    `App listening on port ${process.env.NODE_ENV} mode on port ${PORT}!`,
   );
 });
