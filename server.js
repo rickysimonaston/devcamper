c7135e7a2c32054da6c94b61f2e1bfc2c5caf645
Ricky Aston <rickysimonaston@gmail.com>
2020-02-07 21:26:01 +1000

    End of Section 4 - Full Bootcamp CRUD

--- server.js
+++ server.js
@@ -2,19 +2,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 
 // Own App requirements
-
-//Route Files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 // Load ENV vars
 dotenv.config({
   path: './config/config.env',
 });
 
+// Connect to database
+connectDB();
+
+//Route Files
+const bootcamps = require('./routes/bootcamps');
+
 // Create express app
 const app = express();
+
+// Body Parser
+app.use(express.json());
 
 // Dev logging Middleware
 if (process.env.NODE_ENV === 'development') {
@@ -26,8 +34,17 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 // Create express server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const server = app.listen(
+  PORT,
   console.log(
-    `App listening on port ${process.env.NODE_ENV} mode on port ${PORT}!`,
-  );
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`.yellow
+    .bold,
+  ),
+);
+
+// Handle Unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // close server & exit process
+  server.close(() => process.exit(1));
 });
